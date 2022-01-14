8dc6f9c7bdb10fd7b9d19baba29a158193bf2285
Umar AB <uabadami@gmail.com>
2022-01-14 00:11:18 -0500

    Added heroku files

--- covidtracker/settings.py
+++ covidtracker/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import django_heroku
+
 from pathlib import Path
 from .secret_settings import SECRET_KEY
 
@@ -122,3 +124,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
